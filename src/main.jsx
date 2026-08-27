d438e6d00a79dcf3c30c68b99ccb044c5370fbb0
280468-ops <mydiscofox@gmx.de>
2026-08-28 01:04:40 +0200

    Add closing brace for component

--- src/main.jsx
+++ src/main.jsx
@@ -94,6 +94,7 @@ class ErrorBoundary extends React.Component {
 
     return this.props.children;
   }
+}
 function App() {
   const [session, setSession] = useState(null);
   const [loading, setLoading] = useState(true);
